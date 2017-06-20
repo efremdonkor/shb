45e4ce41154c6ea1b09f602d38438b0dfc862dcd
MaxwellDonkor <MaxwellDonkor@DESKTOP-C2D5IF9.home>
2017-06-20 17:34:30 -0400

    Tuesday Afternoon media Queries Commit

--- index.php
+++ index.php
@@ -7,11 +7,52 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0; maximum-scale=1.0;  user-scalable=0;">
+        <title>SHB :: The Best for the DMV event hosting</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/styles.css"> 
+        <link rel="stylesheet" href="css/media.css"> 
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:700" rel="stylesheet">
+        <script
+            src="https://code.jquery.com/jquery-3.2.1.js"
+            integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
+        crossorigin="anonymous"></script>
+        <script src="js/bootstrap.js"></script>
     </head>
     <body>
         <?php
         // put your code here
         ?>
+        <div class="section">
+            <div class="container">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php">SHB</a>
+                    </div>
+                    <div id="navbar" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav navbar-right text-uppercase">
+                            <li class="active"><a href="index.php">Welcome</a></li>
+                            <li class="active"><a href="about.php">The Boy</a></li>
+                            <li class="active"><a href="services.php">Services</a></li>
+                            <li class="active"><a href="contact.php">Hit me !!</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="col-md-8" id="video">
+                <iframe src="https://www.youtube.com/embed/RGtMXx0mXkw?autoplay=1" frameborder="0" allowfullscreen></iframe>
+            </div>
+            <div class="col-md-4">
+                
+            </div>
+        </div>
     </body>
 </html>
