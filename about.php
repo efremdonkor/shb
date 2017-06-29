5a179ba17607b107f0260a08cc52ec8234191212
DonMaxi <DonMaxi@10.0.0.105>
2017-06-28 22:28:37 -0400

    Merge origin/master
    Conflicts:
    	shm/index.php

--- about.php
+++ about.php
@@ -6,3 +6,17 @@
  * and open the template in the editor.
  */
 
+include 'includes/header.php';
+
+?>
+
+<div class="container-fluid box">
+    <div class="col-md-7 text-uppercase">
+        <h3 class="text-center text-muted" id="desc">
+            <blockquote>He is the Boy with the Sharpest words to draw your audience attention and communicates right.</blockquote>
+        </h3>
+    </div>
+    <div class="col-lg-5">
+        <img src="images/img_9915.png" class="img-rounded img-responsive" >
+    </div>
+</div> 
